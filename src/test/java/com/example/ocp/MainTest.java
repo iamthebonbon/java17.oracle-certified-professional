4618629904f58c07ccd0862733ca0acf3e354b2c
bonbon <n.u.abdysamat@gmail.com>
2026-07-13 15:10:46 +0600

    collec public api

--- src/test/java/com/example/ocp/MainTest.java
+++ src/test/java/com/example/ocp/MainTest.java
@@ -2,10 +2,7 @@ package com.example.ocp;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,28 +18,6 @@ class MainTest {
                 .count();
 
         assertEquals(3, numberOfWords);
-    }
-
-
-    @Test
-    void filtersSquaresAndRemovesDuplicates() {
-        List<Integer> numbers = List.of(-1, 0, 1, -2, 2, 3, -4, 5, -6);
-
-        List<Integer> resultList = numbers.stream()
-                .filter(n -> n < 4)
-                .map(n -> n * n)
-                .filter(n -> n < 10)
-                .collect(Collectors.groupingBy(v->v, Collectors.counting()))
-                .entrySet()
-                .stream()
-                .sorted()
-                .distinct()
-                .reduce(0L, (x,y)->{
-                    return x+y;
-                })
-                .toList();
-
-        assertEquals(List.of(1, 0, 4, 9), resultList);
     }
 
 }
---
+++ src/test/java/com/example/ocp/hyperskill/FunctionalProgrammingTest.java
@@ -0,0 +1,107 @@
+package com.example.ocp.hyperskill;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FunctionalProgrammingTest {
+    private static final Logger LOGGER = Logger.getLogger(FunctionalProgrammingTest.class.getName());
+
+    /**
+     * https://hyperskill.org/learn/step/2449
+     */
+    @Test
+    public void streamPipelinesTest() {
+        String input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.";
+        Stream.of(input.toLowerCase().replaceAll("[^A-Za-z0-9 ]", "").split(" "))
+                .collect(
+                        (Supplier<Map<String, Long>>) HashMap::new,
+                        (supplier, element) -> supplier.merge(element, 1L, Long::sum),
+                        (combineOne, combineTwo) -> combineOne.putAll(combineTwo)
+                )
+                .entrySet()
+                .stream()
+                .sorted((i1, i2) -> {
+                    if (i1.getValue().equals(i2.getValue())) {
+                        return i1.getKey().compareTo(i2.getKey());
+                    }
+                    return i2.getValue().compareTo(i1.getValue());
+                })
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .filter(v -> {
+                    return true;
+                })
+                .collect(
+                        new Supplier<List<String>>() {
+                            @Override
+                            public List<String> get() {
+                                LOGGER.info("Supplier here");
+                                return new ArrayList<>();
+                            }
+                        },
+                        new BiConsumer<List<String>, String>() {
+                            @Override
+                            public void accept(List<String> strings, String s) {
+                                LOGGER.info("Accumulator here");
+                                strings.add(s);
+                            }
+                        },
+                        new BiConsumer<List<String>, List<String>>() {
+                            @Override
+                            public void accept(List<String> strings, List<String> strings2) {
+                                LOGGER.info("Combiner here");
+                                strings.addAll(strings2);
+                            }
+                        }
+                )
+                .forEach(System.out::println);
+    }
+
+    /**
+     * https://hyperskill.org/learn/step/2281
+     */
+    @Test
+    public void test2() {
+        String number = "090234";
+        var array = number.split("");
+
+        System.out.println(
+                Arrays.stream(Arrays.copyOfRange(array, 0, 3))
+                        .parallel()
+                        .map(Integer::parseInt)
+                        .collect(
+                                Collector.of(
+                                        AtomicInteger::new,
+                                        AtomicInteger::addAndGet,
+                                        (atomicInteger, atomicInteger2) -> {
+                                            atomicInteger.addAndGet(atomicInteger2.get());
+                                            return atomicInteger;
+                                        },
+                                        AtomicInteger::get
+                                )
+                        )
+        );
+
+    }
+
+    @Test
+    public void test() {
+        Stream.of(1, 2, 3)
+                .collect(Collectors.toSet());
+    }
+
+}
