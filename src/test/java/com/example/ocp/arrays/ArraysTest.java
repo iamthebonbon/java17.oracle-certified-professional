c84200200432a838756b3e1059f04f6f0d3e6771
bonbon <n.u.abdysamat@gmail.com>
2026-07-14 13:20:13 +0600

    ArraysTest.java

---
+++ src/test/java/com/example/ocp/arrays/ArraysTest.java
@@ -0,0 +1,132 @@
+package com.example.ocp.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class ArraysTest {
+
+    @Test
+    public void arraysIntTest() {
+        int[] arrayOne = {1, 2, 3};
+        int[] arrayTwo = {1, 2, 3};
+        Assertions.assertFalse(arrayOne.hashCode() == arrayTwo.hashCode());
+        Assertions.assertFalse(arrayOne.equals(arrayTwo));
+        Assertions.assertTrue(Arrays.equals(arrayOne, arrayTwo));
+    }
+
+    @Test
+    public void arraysStringTest() {
+        String[] arrayOne = {"1", "2", "3"};
+        String[] arrayTwo = {"1", "2", "3"};
+        Assertions.assertFalse(arrayOne.hashCode() == arrayTwo.hashCode());
+        Assertions.assertFalse(arrayOne.equals(arrayTwo));
+        Assertions.assertTrue(Arrays.equals(arrayOne, arrayTwo));
+    }
+
+    @Test
+    public void arraysCompareTest() {
+        Assertions.assertEquals(0,
+                Arrays.compare(
+                        new String[]{"1", "2", "3"},
+                        new String[]{"1", "2", "3"}
+                )
+        );
+        Assertions.assertEquals(-1,
+                Arrays.compare(
+                        new String[]{"1", "2", "3"},
+                        new String[]{"2", "2", "3"}
+                )
+        );
+        Assertions.assertEquals(1,
+                Arrays.compare(
+                        new String[]{"2", "2", "3"},
+                        new String[]{"1", "2", "3"}
+                )
+        );
+    }
+
+    @Test
+    public void arraysCopyOfTest() {
+        Assertions.assertTrue(
+                Arrays.equals(
+                        new String[]{"1", "2"},
+                        Arrays.copyOf(
+                                new String[]{"1", "2", "3", "4"},
+                                2
+                        )
+                )
+        );
+        Assertions.assertTrue(
+                Arrays.equals(
+                        new int[]{1, 2, 3, 4},
+                        Arrays.copyOf(
+                                new int[]{1, 2, 3, 4, 5, 6},
+                                4
+                        )
+                )
+        );
+    }
+
+    @Test
+    public void arraysCopyOfRangeTest() {
+        Assertions.assertTrue(
+                Arrays.equals(
+                        new String[]{"3"},
+                        Arrays.copyOfRange(
+                                new String[]{"1", "2", "3", "4"},
+                                2, 3
+                        )
+                )
+        );
+        Assertions.assertTrue(
+                Arrays.equals(
+                        new String[]{"3", "4"},
+                        Arrays.copyOfRange(
+                                new String[]{"1", "2", "3", "4"},
+                                2, 4
+                        )
+                )
+        );
+        Assertions.assertTrue(
+                Arrays.equals(
+                        new String[]{"3", "4", null},
+                        Arrays.copyOfRange(
+                                new String[]{"1", "2", "3", "4"},
+                                2, 5
+                        )
+                )
+        );
+    }
+
+    @Test
+    public void arraysFillTest() {
+        String[] strings = {"1", "2", "3", "4"};
+        Arrays.fill(
+                strings,
+                1,
+                3,
+                "5"
+        );
+        Assertions.assertTrue(
+                Arrays.equals(
+                        new String[]{"1", "5", "5", "4"},
+                        strings
+                )
+        );
+    }
+
+    @Test
+    public void arraysAsListTest() {
+        Assertions.assertEquals(
+                "[1, 5, 5, 4]",
+                Arrays.toString(new int[]{1, 5, 5, 4})
+        );
+        Assertions.assertEquals(
+                "[1, 5, 5, 4]",
+                Arrays.toString(new String[]{"1", "5", "5", "4"})
+        );
+    }
+
+}
