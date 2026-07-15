48464b67c52d451bb63e8d6144bc08ebe813cff7
bonbon <n.u.abdysamat@gmail.com>
2026-07-15 22:17:41 +0600

    ArrowSyntaxExpressionTest.java

---
+++ src/test/java/com/example/ocp/switches/ArrowSyntaxStatementTest.java
@@ -0,0 +1,51 @@
+package com.example.ocp.switches;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class ArrowSyntaxStatementTest {
+
+    @MethodSource
+    @ParameterizedTest
+    public void test(
+            int expected,
+            String input
+    ) {
+        if (expected > 5) {
+            Assertions.assertThrows(
+                    IllegalArgumentException.class, () -> getSwitch(input)
+            );
+        } else {
+            Assertions.assertEquals(expected, getSwitch(input));
+        }
+    }
+
+    private int getSwitch(String value) {
+        int result;
+        switch (value) {
+            case "Mon", "Tue", "Wed" -> {
+                result = 3;
+            }
+            case "Fri" -> {
+                result = 5;
+            }
+            default -> throw new IllegalArgumentException();
+        }
+        return result;
+    }
+
+    static Stream<Arguments> test() {
+        return Stream.of(
+                Arguments.of(3, "Mon"),
+                Arguments.of(3, "Tue"),
+                Arguments.of(3, "Wed"),
+                Arguments.of(5, "Fri"),
+                Arguments.of(6, "Sat")
+        );
+    }
+
+}
