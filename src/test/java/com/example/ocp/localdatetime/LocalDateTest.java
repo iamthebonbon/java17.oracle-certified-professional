fff5f8a4e791d8c288fda7631093fd848d48091b
bonbon <n.u.abdysamat@gmail.com>
2026-07-16 13:53:03 +0600

    LocalTimeTest.java

--- src/test/java/com/example/ocp/localdatetime/LocalDateTest.java
+++ src/test/java/com/example/ocp/localdatetime/LocalDateTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 
 import static java.time.DayOfWeek.MONDAY;
 
@@ -32,9 +34,29 @@ public class LocalDateTest {
     @Test
     public void testDatesUtil() {
         LocalDate localDate = LocalDate.of(2017, 1, 1);
-        localDate.datesUntil(localDate.plusMonths(1))
+        ArrayList<LocalDate> collect = localDate.datesUntil(localDate.plusMonths(1))
                 .filter(v -> v.getDayOfWeek() == MONDAY)
-                .forEach(System.out::println);
+                .collect(
+                        ArrayList::new,
+                        ArrayList::add,
+                        (c1, c2) -> {
+                        }
+                );
+        Assertions.assertEquals("[2017-01-02, 2017-01-09, 2017-01-16, 2017-01-23, 2017-01-30]", collect.toString());
+    }
+
+    @Test
+    public void testDatesUtilWithOffset() {
+        LocalDate localDate = LocalDate.of(2017, 1, 1);
+        ArrayList<LocalDate> collect =
+                localDate.datesUntil(localDate.plusMonths(1), Period.ofDays(7))
+                        .collect(
+                                ArrayList::new,
+                                ArrayList::add,
+                                (c1, c2) -> {
+                                }
+                        );
+        Assertions.assertEquals("[2017-01-01, 2017-01-08, 2017-01-15, 2017-01-22, 2017-01-29]", collect.toString());
     }
 
 }
