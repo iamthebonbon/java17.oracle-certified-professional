0eb6b9e39335b6e06b9a7b742afba582e927b407
bonbon <n.u.abdysamat@gmail.com>
2026-07-20 14:45:30 +0600

    JdbcTest

--- src/test/java/com/example/ocp/jdbc/JdbcTest.java
+++ src/test/java/com/example/ocp/jdbc/JdbcTest.java
@@ -1,9 +1,11 @@
 package com.example.ocp.jdbc;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import java.sql.Connection;
@@ -12,11 +14,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-@Testcontainers
 public class JdbcTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcTest.class);
 
-    @Container
-    private static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine");
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine");
+
+    @BeforeAll
+    public static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        postgres.stop();
+    }
 
     @Test
     void connectsAndQueries() throws SQLException {
@@ -30,6 +41,21 @@ public class JdbcTest {
             ResultSet rs = stmt.executeQuery("SELECT name FROM employees");
             Assertions.assertTrue(rs.next());
             Assertions.assertEquals("Alice", rs.getString("name"));
+        }
+    }
+
+    @Test
+    void test() {
+        try (
+                Connection conn = DriverManager.getConnection(postgres.getJdbcUrl(), postgres.getUsername(), postgres.getPassword());
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT name FROM employees")
+        ) {
+            Assertions.assertTrue(rs.next());
+        } catch (Exception e) {
+            LOGGER.info(e.getMessage());
+        } finally {
+            LOGGER.info("I'm here");
         }
     }
 
