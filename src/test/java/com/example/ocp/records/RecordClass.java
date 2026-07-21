cea2ad20b8fb278ba31fe5380d30971894b0cc4e
bonbon <n.u.abdysamat@gmail.com>
2026-07-21 15:38:14 +0600

    RecordClass.java

--- src/test/java/com/example/ocp/records/RecordClass.java
+++ src/test/java/com/example/ocp/records/RecordClass.java
@@ -1,6 +1,11 @@
 package com.example.ocp.records;
 
 public record RecordClass(int id, String name) implements RecordClassInterface {
+    static int instanceCount = 0;
+
+    public RecordClass {
+        System.out.println(instanceCount);
+    }
 
     @Override
     public String name() {
