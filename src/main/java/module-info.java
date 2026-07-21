78a2d5ad55379b33ce4f7a4fab5de2d0e4cbe7c7
bonbon <n.u.abdysamat@gmail.com>
2026-07-21 18:57:21 +0600

    module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,5 @@
-module com.example.java.oracle.certified.professional {
+module _$123com.example.java.oracle.certified.professional {
     requires java.sql;
 }
+// First character	Letter, $, or _
+// Subsequent characters	Letters, digits, $, _
