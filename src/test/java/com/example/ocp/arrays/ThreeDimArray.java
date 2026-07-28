1135196b36093a94ad7b906ba72d4ed20b296cb2
bonbon <n.u.abdysamat@gmail.com>
2026-07-28 17:18:46 +0600

    ThreeDimArray.java

--- src/test/java/com/example/ocp/arrays/ThreeDimArray.java
+++ src/test/java/com/example/ocp/arrays/ThreeDimArray.java
@@ -1,6 +1,47 @@
 package com.example.ocp.arrays;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ThreeDimArray {
 
+    @Test
+    public void test() {
+        String[][][] array = {};
+        Assertions.assertTrue(array.length == 0);
+        Assertions.assertThrows(
+                ArrayIndexOutOfBoundsException.class,
+                () -> {
+                    String[][] strings = array[0];
+                }
+        );
+    }
+
+
+    @Test
+    public void test2() {
+        String[][][] array = {{}, {}, {}};
+        Assertions.assertTrue(array.length == 3);
+        Assertions.assertTrue(array[0].length == 0);
+        Assertions.assertThrows(
+                ArrayIndexOutOfBoundsException.class,
+                () -> {
+                    String[] strings = array[0][0];
+                }
+        );
+    }
+
+    @Test
+    public void test3() {
+        String[][][] arr = {
+                {{"a", "b", "c"}, {"d", "e", null}},
+                {{"x"}, null}, {{"y"}}, {{"z", "p"}, {}}, {}
+        };
+        Assertions.assertTrue(5 == arr.length);
+        Assertions.assertTrue("a".equals(arr[0][0][0]));
+        Assertions.assertTrue(null == (arr[0][1][2]));
+        Assertions.assertTrue(0 == (arr[3][1].length));
+        Assertions.assertTrue(0 == arr[4].length);
+    }
 
 }
