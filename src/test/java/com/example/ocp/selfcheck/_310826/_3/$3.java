0b120f5c82adaf81b6e188c10d5d409cccbcbdf3
anamazbek-uulu <anamazbek-uulu@cloudlinux.com>
2026-08-31 17:41:38 +0600

    self check

--- src/test/java/com/example/ocp/selfcheck/_310826/_3/$3.java
+++ src/test/java/com/example/ocp/selfcheck/_310826/_3/$3.java
@@ -8,36 +8,52 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.UnsupportedTemporalTypeException;
 
 public class $3 {
 
     @Test
     public void test() {
-        Instant instant = Instant.parse("2026-08-31T05:00:00+05:00");
-
         Assertions.assertThrows(
-                UnsupportedTemporalTypeException.class,
-                () -> LocalDate.ofInstant(instant, ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ss"))
+                DateTimeParseException.class,
+                () -> Instant.parse("2026-08-31T05:00:00")
         );
 
         Assertions.assertThrows(
                 UnsupportedTemporalTypeException.class,
-                () -> LocalDateTime.ofInstant(instant, ZoneOffset.UTC).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ssZ"))
+                () -> LocalDate.ofInstant(
+                        Instant.parse("2026-08-31T05:00:00+05:00"),
+                        ZoneOffset.UTC
+                ).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ss"))
+        );
+
+        Assertions.assertThrows(
+                UnsupportedTemporalTypeException.class,
+                () -> LocalDateTime.ofInstant(
+                        Instant.parse("2026-08-31T05:00:00+05:00"),
+                        ZoneOffset.UTC
+                ).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ssZ"))
         );
 
         Assertions.assertEquals(
                 "2026-08-31T00:00:00Z",
-                instant + ""
+                Instant.parse("2026-08-31T05:00:00+05:00") + ""
         );
 
         Assertions.assertEquals(
                 "26-08-31T05:00:00",
-                LocalDateTime.ofInstant(instant, ZoneOffset.of("+05:00")).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ss"))
+                LocalDateTime.ofInstant(
+                        Instant.parse("2026-08-31T05:00:00+05:00"),
+                        ZoneOffset.of("+05:00")
+                ).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ss"))
         );
         Assertions.assertEquals(
                 "26-08-30T19:00:00",
-                LocalDateTime.ofInstant(instant, ZoneOffset.of("-05:00")).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ss"))
+                LocalDateTime.ofInstant(
+                        Instant.parse("2026-08-31T05:00:00+05:00"),
+                        ZoneOffset.of("-05:00")
+                ).format(DateTimeFormatter.ofPattern("yy-MM-dd'T'HH:mm:ss"))
         );
     }
 
